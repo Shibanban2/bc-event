545f4039f70c5dbd4018d0aa150ed41badd8369e
Shibanban2 <salenyanko12891745@gmail.com>
2025-09-24 16:42:06 +0900

    Update generate_update_list.py

--- scripts/generate_update_list.py
+++ scripts/generate_update_list.py
@@ -32,7 +32,7 @@ def main():
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>更新ステージ一覧</title>
+  <title>Ver.14.7</title>
   <link rel="icon" href="favicon.ico" type="image/x-icon" />
 
   <!-- Google tag (gtag.js) -->
